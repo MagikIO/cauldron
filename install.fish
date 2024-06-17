d44dce1f79454d767188bc78605ad03d37d1df6f
Abourass <Abourassa@AssetVal.com>
2024-06-17 15:37:32 -0600

    feat: Add cpfunc to install.fish script
    This commit adds the `cpfunc` function to the `install.fish` script. The `cpfunc` function is used to copy functions to the appropriate directory and make them executable. This addition ensures that the `cpfunc` function is available for use in the installation process.
    
    Note: This commit message does not include any meta information like issue references, tags, or author names.

--- install.fish
+++ install.fish
@@ -7,75 +7,47 @@ if not set -q CAULDRON_PATH
     set -Ux CAULDRON_PATH $HOME/.config/cauldron
 end
 
-# Where we are installing from
-set CAULDRON_SETUP_PATH (dirname (status -f))
-
 # List of folders with functions
-set CAULDRON_LOCAL_DIRS bin alias config familiar functions text ui
+set -g CAULDRON_LOCAL_DIRS bin alias config familiar functions text UI
 
-# Now we need to install the dependencies
-set CAULDRON_DEPENDENCIES $CAULDRON_PATH/setup/dependencies.json
-set CAULDRON_BASE_PALETTES $CAULDRON_PATH/setup/palettes.json
-set CAULDRON_BASE_DB $CAULDRON_PATH/setup/cauldron.db
+# First we need to make sure we have cpfunc installed
+if not command -q cpfunc
+    cp ./functions/cpfunc.fish ~/.config/fish/functions/cpfunc.fish
+    chmod +x ~/.config/fish/functions/cpfunc.fish
+end
 
 # We have to patch their version of `installs` with our version
 cpfunc ./functions/ -d
 
-# Prep the environment for the installation
-
-# Check if the sll base files are found
-if test -f $CAULDRON_DEPENDENCIES -a -f $CAULDRON_BASE_PALETTES -a -f $CAULDRON_BASE_DB
-    # And if so, we should quit if they share the same version
-    set INSTALLED_VERSION (jq -r '.version' $CAULDRON_DEPENDENCIES)
-    set LOCAL_VERSION (jq -r '.version' $CAULDRON_SETUP_PATH/dependencies.json)
-
-    if test $INSTALLED_VERSION = $LOCAL_VERSION
-        printf "Cauldron's files are up to date. \nChecking if dependencies need updated..\n\n"
+for dir in $CAULDRON_LOCAL_DIRS
+    if not test -d $CAULDRON_PATH/$dir
+        mkdir -p $CAULDRON_PATH/$dir
     end
-else 
-    printf "Cauldron's base install files are not found. \nPlease re-pull the latest release from our github page and try again\n\n"
-    return 1
-end
 
-# Create the directory structure
-mkdir -p $CAULDRON_PATH
-mkdir -p $CAULDRON_PATH/bin
-mkdir -p $CAULDRON_PATH/commands
-mkdir -p $CAULDRON_PATH/config
-mkdir -p $CAULDRON_PATH/data
-mkdir -p $CAULDRON_PATH/logs
-mkdir -p $CAULDRON_PATH/scripts
-mkdir -p $CAULDRON_PATH/tmp
-mkdir -p $CAULDRON_PATH/tools
-
-# Create the configuration files
-if not test -f $CAULDRON_PATH/config/cauldron.json
-    cp $CAULDRON_SETUP_PATH/cauldron.json $CAULDRON_PATH/config/cauldron.json
-end
-
-# If they dont have `palettes.json` yet move it over
-if not test -f $CAULDRON_PATH/config/palettes.json
-    cp $CAULDRON_SETUP_PATH/palettes.json $CAULDRON_PATH/config/palettes.json
+    cpfunc ./$dir -d
 end
 
 # Create the log files
 if not test -f $CAULDRON_PATH/logs/cauldron.log
-    touch $CAULDRON_PATH/logs/cauldron.log
+    touch ./logs/cauldron.log
 end
 
-# Create the data files
-if not test -f $CAULDRON_PATH/data/cauldron.db
-    touch $CAULDRON_PATH/data/cauldron.db
+# Move over the palettes and cauldron.
+mkdir -p $CAULDRON_PATH/user
+cp ./setup/palettes.json $CAULDRON_PATH/user/palettes.json
+cp ./setup/cauldron.db $CAULDRON_PATH/user/cauldron.db
+
+# If pipx not installed 
+if not command -q pipx
+    sudo apt install pipx
+    pipx ensurepath
+    register-python-argcomplete --shell fish pipx >~/.config/fish/completions/pipx.fish
+    pipx install terminaltexteffects
 end
 
-# Copy the dependencies file
-cp $CAULDRON_SETUP_PATH/dependencies.json $CAULDRON_DEPENDENCIES
-
-# Loop through each function in the bin folder and copy it over
-for file in (ls $CAULDRON_LOCAL_BIN_FNs)
-    cp $CAULDRON_LOCAL_BIN_FNs/$file $CAULDRON_PATH/bin/$file
+if not command -q gum
+    brew install gum
 end
-
 
 
 function cauldron_mise_en_place
@@ -89,9 +61,9 @@ function cauldron_mise_en_place
     #      "lolcat-c"
     #    ],
     # }
-    set -l APT_DEPENDENCIES (jq -r '.apt[]' $CAULDRON_SETUP_PATH/dependencies.json)
-    set -l BREW_DEPENDENCIES (jq -r '.brew[]' $CAULDRON_SETUP_PATH/dependencies.json)
-    set -l SNAP_DEPENDENCIES (jq -r '.snap[]' $CAULDRON_SETUP_PATH/dependencies.json)
+    set -l APT_DEPENDENCIES (jq -r '.apt[]' ./setup/dependencies.json)
+    set -l BREW_DEPENDENCIES (jq -r '.brew[]' ./setup/dependencies.json)
+    set -l SNAP_DEPENDENCIES (jq -r '.snap[]' ./setup/dependencies.json)
 
     # Install the dependencies
     echo -e (badge green 'APT') (underline 'Installing')": $APT_DEPENDENCIES \n"
@@ -107,9 +79,5 @@ function cauldron_mise_en_place
     return 0
 end
 
-
-
-
-cauldron_plating
 cauldron_mise_en_place
 return 0
