98935649a4b0c0e1d0f606fd0ff9ae3586b986da
Abourass <Abourassa@AssetVal.com>
2024-06-26 15:01:55 -0600

    feat: Add install_aquarium.fish script to install the Aquarium CLI
    This commit adds the install_aquarium.fish script, which is responsible for installing the Aquarium CLI. The script checks if the Aquarium CLI is already installed and clones the repository from GitHub if it is not. It then runs the installation script to set up the Aquarium CLI. This addition enhances the project by providing an easy way to install and use the Aquarium CLI.

---
+++ setup/install_aquarium.fish
@@ -0,0 +1,13 @@
+#!/usr/bin/env fish
+
+function install_aquarium -d 'Install the Aquarium CLI'
+  # Check if aquarium is installed
+  if not type -q aquarium
+    print_center "🐠 Filling Aquarium 🐠"
+    rm -rf ~/.cache/aquarium
+    git clone --depth 1 https://github.com/anandamideio/aquarium.git ~/.cache/aquarium
+    pushd ~/.cache/aquarium/bin/
+    ./install
+    popd
+  end
+end
