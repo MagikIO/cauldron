27b0041cd57f7033bb5b62bef08157f50d2dfa60
Abourass <Abourassa@AssetVal.com>
2024-06-26 16:38:30 -0600

    chore: Marked styled-banner as not ready for release

--- text/styled-banner.fish
+++ text/styled-banner.fish
@@ -1,7 +1,7 @@
 function styled-banner --argument txt
 
-    set future_banner_fonts smbraille
-    clear
+    # set future_banner_fonts smbraille
+    # clear
 
-
+echo "This command is not yet ready for use."
 end
