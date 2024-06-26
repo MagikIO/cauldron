e8a02224a34c184c8fb409999f409fd6cea1554c
Abourass <Abourassa@AssetVal.com>
2024-06-26 15:01:03 -0600

    Refactor n+ function to use asdf or nvm based on user preference
    This commit modifies the n+ function in the alias/n+.fish file to use either asdf or nvm based on the user's preference. The function now checks the value of the "cauldron_packman_pref" variable and executes the corresponding update command for either asdf or nvm. This change enhances the flexibility of the n+ function by allowing users to choose their preferred package manager for updating Node and Yarn to the latest version.

--- alias/n+.fish
+++ alias/n+.fish
@@ -1,6 +1,6 @@
 #!/usr/bin/env fish
 
-function n+ --description 'Get Node and Yarn to the lastest version (Using abbr)'
+function n+ --description 'Get Node and Yarn to the lastest version (Using asdf)'
     # Check if there is a package.json file (If not we should prompt them if they are use they meant to use this in this folder)
     if test ! -f package.json
         confirm "This folder does not have a package.json file. Are you sure you want to continue?"
@@ -9,18 +9,19 @@ function n+ --description 'Get Node and Yarn to the lastest version (Using abbr)
         end
     end
 
+    if test $cauldron_packman_pref = "asdf"
+        asdf_update_node
+    else if test $cauldron_packman_pref = "nvm"
+        nvm_update_node
+    end
 
-
-    asdf install nodejs latest
-    asdf global nodejs latest
-    asdf local nodejs latest
-    corepack enable
-    asdf reshim nodejs
-    yarn set version stable
-    asdf reshim nodejs
-    yarn
-    npm i tsx@latest -g
     yarn upgrade-interactive
-    asdf reshim nodejs
+
+    if test $cauldron_packman_pref = "asdf"
+      asdf reshim nodejs
+    else if test $cauldron_packman_pref = "nvm"
+      nvm_update_node
+    end
+    
     $argv
 end
