1d7b21e932659e054764db06e32aec09208a9c98
Abourass <Abourassa@AssetVal.com>
2024-06-26 18:25:17 -0600

    feat Banners

--- text/black-hole-banner.fish
+++ text/black-hole-banner.fish
@@ -1,3 +1,5 @@
+#!/usr/bin/env fish
+
 function black-hole-banner --argument txt
 banner $txt | tte blackhole --star-colors ffcc0d ff7326 ff194d bf2669 702a8c 049dbf --final-gradient-stops 8A008A 00D1FF FFFFFF --final-gradient-steps 12 --final-gradient-direction vertical
 end
--- text/styled-banner.fish
+++ text/styled-banner.fish
@@ -1,7 +1,19 @@
 function styled-banner --argument txt
-
-    # set future_banner_fonts smbraille
-    # clear
-
-echo "This command is not yet ready for use."
+    # IF the $CAULDRON_BANNER_ANIMATION is set we should use the corresponding animation
+    if not set -q "$CAULDRON_BANNER_ANIMATION"
+        banner $txt | rain-effect
+    else
+        switch $CAULDRON_BANNER_RENDER_OPTION
+            case rain
+                banner $txt | rain-effect
+            case orbital-volley
+                banner $txt | orbital-volley-effect
+            case vhs
+                banner $txt | vhs-effect
+            case beam
+                banner $txt | beam-effect
+            case black-hole
+                banner $txt | black-hole-effect
+        end
+    end
 end
