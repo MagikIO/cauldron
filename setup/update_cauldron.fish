98fd6d5ef9131536fbbd4db5425de13aee6ad702
Abourass <Abourassa@AssetVal.com>
2024-07-01 10:16:09 -0600

    Refactor update_cauldron.fish to improve code readability and add initial setup for first-time users

--- setup/update_cauldron.fish
+++ setup/update_cauldron.fish
@@ -1,5 +1,4 @@
 function update_cauldron
-    # getLatestGithubReleaseAsJSON MagikIO/cauldron >~/.config/cauldron/latestRelease.json
     # Check if ~/.config/cauldron/latestRelease.json exists
     if test -f ~/.config/cauldron/latestRelease.json
         # Now we use the getLatestGithubReleaseAsJSON function to get the latest release to compare against
@@ -21,5 +20,18 @@ function update_cauldron
             echo "🔮 Cauldron Update Available 🔮" | rain-effect
             echo $changelog | shiny pager
         end
+    else
+        # They have never run the update tool before, so lets run it for them
+        # Store a version of the latest release in a temp file
+        set incomingFileLocation ~/.config/cauldron/_temp_latestRelease.json
+        # Generate a new temp file with the latest release
+        getLatestGithubReleaseAsJSON MagikIO/cauldron -s >$incomingFileLocation
+        # Get the version of the incoming release
+        set incomingVersion (cat $incomingFileLocation | jq -r '.tag_name')
+        # We should prompt the user and ask if we can update the Aquarium CLI
+        # As well as show them a brief changelog, from the incoming release
+        set changelog (cat $incomingFileLocation | jq -r '.body')
+        banner "Update Avail!" | rain-effect
+        echo $changelog | shiny pager
     end
 end
