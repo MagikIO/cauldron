977428313f6485927d9e0306923df42f77dba7ec
Abourass <Abourassa@AssetVal.com>
2024-07-09 09:08:41 -0600

    chore: Refactor install.fish to use INSERT OR REPLACE for updating dependencies table

--- install.fish
+++ install.fish
@@ -85,21 +85,23 @@ set brew_dependencies (cat ./dependencies.json | jq -r '.brew[]')
 set snap_dependencies (cat ./dependencies.json | jq -r '.snap[]')
 
 # SQL command to create the 'dependencies' table if it doesn't exist
-set CREATE_TABLE_SQL "CREATE TABLE IF NOT EXISTS dependencies (id INTEGER PRIMARY KEY, name TEXT, version TEXT, date TEXT);"
+set CREATE_TABLE_SQL "CREATE TABLE IF NOT EXISTS dependencies (id INTEGER PRIMARY KEY, name TEXT UNIQUE, version TEXT, date TEXT);"
 
 # Execute the CREATE TABLE command
 sqlite3 $CAULDRON_DATABASE $CREATE_TABLE_SQL
 
 for dep in $apt_dependencies
-  gum spin --spinner moon --title "Installing $dep..." -- fish -c "if not command -q \$dep; sudo apt install \$dep -y; end; if not command -q \$dep; set ERROR_MSG \"Failed to install: \$dep using the command 'sudo apt install \$dep -y'\"; echo \$ERROR_MSG >> \$CAULDRON_PATH/logs/cauldron.log; else; set VERSION (apt show \$dep | grep \"Version\" | cut -d \":\" -f 2 | tr -d \" \"); set DATE (date); sqlite3 \$CAULDRON_DATABASE \"INSERT INTO dependencies (name, version, date) VALUES ('\$dep', '\$VERSION', '\$DATE')\"; end"
+  gum spin --spinner moon --title "Installing $dep..." -- fish -c "if not command -q \$dep; sudo apt install \$dep -y; end; if not command -q \$dep; set ERROR_MSG \"Failed to install: \$dep using the command 'sudo apt install \$dep -y'\"; echo \$ERROR_MSG >> \$CAULDRON_PATH/logs/cauldron.log; else; set VERSION (apt show \$dep | grep \"Version\" | cut -d \":\" -f 2 | tr -d \" \"); set DATE (date); sqlite3 \$CAULDRON_DATABASE \"INSERT OR REPLACE INTO dependencies (name, version, date) VALUES ('\$dep', '\$VERSION', '\$DATE')\"; end"
 end
 
 for dep in $brew_dependencies
-  gum spin --spinner moon --title "Installing $dep..." -- fish -c "if not command -q \$dep; brew install \$dep; end; if not command -q \$dep; set ERROR_MSG \"Failed to install: \$dep using the command 'brew install \$dep'\"; echo \$ERROR_MSG >> \$CAULDRON_PATH/logs/cauldron.log; else; set VERSION (brew info \$dep | grep \"version\" | cut -d \" \" -f 1 | tr -d \"version:\"); set DATE (date); sqlite3 \$CAULDRON_DATABASE \"INSERT INTO dependencies (name, version, date) VALUES ('\$dep', '\$VERSION', '\$DATE');\"; end"
+  gum spin --spinner moon --title "Installing $dep..." -- fish -c "if not command -q \$dep; brew install \$dep; end; if not command -q \$dep; set ERROR_MSG \"Failed to install: \$dep using the command 'brew install \$dep'\"; echo \$ERROR_MSG >> \$CAULDRON_PATH/logs/cauldron.log; else; set VERSION (brew info \$dep | grep \"version\" | cut -d \" \" -f 1 | tr -d \"version:\"); set DATE (date); sqlite3 \$CAULDRON_DATABASE \"INSERT OR REPLACE INTO dependencies (name, version, date) VALUES ('\$dep', '\$VERSION', '\$DATE');\"; end"
 end
 
 for dep in $snap_dependencies
-  gum spin --spinner moon --title "Installing $dep..." -- fish -c "if not command -q \$dep; sudo snap install \$dep; end; if not command -q \$dep; set ERROR_MSG \"Failed to install: \$dep using the command 'sudo snap install \$dep'\"; echo \$ERROR_MSG >> \$CAULDRON_PATH/logs/cauldron.log; else; set VERSION (snap info \$dep | grep \"installed\" | cut -d \":\" -f 2 | tr -d \" \"); set DATE (date); sqlite3 \$CAULDRON_DATABASE \"INSERT INTO dependencies (name, version, date) VALUES ('\$$dep', '\$VERSION', '\$DATE');\"; end"
+  gum spin --spinner moon --title "Installing $dep..." -- fish -c "if not command -q \$dep; sudo snap install \$dep; end; if not command -q \$dep; set ERROR_MSG \"Failed to install: \$dep using the command 'sudo snap install \$dep'\"; echo \$ERROR_MSG >> \$CAULDRON_PATH/logs/cauldron.log; else; set VERSION (snap info \$dep | grep \"installed\" | cut -d \":\" -f 2 | tr -d \" \"); set DATE (date); sqlite3 \$CAULDRON_DATABASE \"INSERT OR REPLACE INTO dependencies (name, version, date) VALUES ('\$$dep', '\$VERSION', '\$DATE');\"; end"
 end
+
+styled-banner "Installed!"
 
 return 0
--- update.fish
+++ update.fish
@@ -1,33 +1,134 @@
 #!/usr/bin/env fish
 
 if not set -q CAULDRON_PATH
-    set -Ux CAULDRON_PATH $HOME/.config/cauldron
-    mkdir -p $CAULDRON_PATH
+  set -Ux CAULDRON_PATH $HOME/.config/cauldron
+end
+if not set -q __CAULDRON_DOCUMENTATION_PATH
+  set -Ux __CAULDRON_DOCUMENTATION_PATH $CAULDRON_PATH/docs
+end
+if not set -q CAULDRON_GIT_REPO
+  set -Ux CAULDRON_GIT_REPO "https://github.com/MagikIO/cauldron.git"
 end
 
-# First lets udpate all of our cauldron functions
-
-# We will start with cpfunc since it is used to install the rest
-if not command -q cpfunc
-    cp ./functions/cpfunc.fish ~/.config/fish/functions/cpfunc.fish
-    chmod +x ~/.config/fish/functions/cpfunc.fish
-    source ~/.config/fish/functions/cpfunc.fish
+# First we need to make sure our destination folder exists
+if not test -d $CAULDRON_PATH
+  mkdir -p $CAULDRON_PATH
 end
 
-# Next we will just update all of our functions
-cpfunc ./cli -d
-cpfunc ./config -d
-cpfunc ./effects -d
-cpfunc ./familiar -d
-cpfunc ./functions -d
-cpfunc ./internal -d
-cpfunc ./text -d
-cpfunc ./UI -d
-cpfunc "./docs/__cauldron_help.fish"
-cpfunc "./docs/__cauldron_install_help.fish"
+set tmp_dir (mktemp -d)
 
-# Now lets copy all of our functions and documentation to the $CAULDRON_PATH except the .git folder
-rsync -av --exclude '.git' --exclude node_modules ./ $CAULDRON_PATH/
+# Next we need to backup their data folder by copying it to a temp folder
+if test -d $CAULDRON_PATH/data
+  mv $CAULDRON_PATH/data $tmp_dir/data
+end
+
+# Now we remove everything in the base folder so we can clone the latest version
+rm -rf $CAULDRON_PATH/*
+
+# Now we clone the latest version of the repo
+git clone $CAULDRON_GIT_REPO $CAULDRON_PATH
+
+# Now we copy the data folder back
+if test -d $tmp_dir/data
+  mv $tmp_dir/data $CAULDRON_PATH/data
+end
+
+# Now we remove the temp folder
+rm -rf $tmp_dir
+
+# List of folders with functions
+set CAULDRON_LOCAL_DIRS "alias" "cli" "config" "effects" "familiar" "internal" "setup" "text" "UI"
 
 
-f-says "Cauldron has been updated to the latest version!"
+# First we need to make sure we have cpfunc installed
+if not functions -q cpfunc
+  cp ./functions/cpfunc.fish ~/.config/fish/functions/cpfunc.fish
+  chmod +x ~/.config/fish/functions/cpfunc.fish
+  source ~/.config/fish/functions/cpfunc.fish
+end
+
+# We have to patch their version of `installs` with our version
+cpfunc ./functions/ -d
+
+# Copy and source all the needed functions
+for dir in $CAULDRON_LOCAL_DIRS
+  if not test -d $CAULDRON_PATH/$dir
+      mkdir -p $CAULDRON_PATH/$dir
+  end
+
+  cp -r ./$dir $CAULDRON_PATH/$dir
+  cpfunc ./$dir -d
+end
+
+# Install the one off scripts that are not part of the main CLI
+cpfunc ./packages/asdf -d
+cpfunc ./packages/nvm -d
+cpfunc ./packages/choose_packman.fish
+
+cp -r ./packages $CAULDRON_PATH/packages
+
+# Now we recursively copy the data, docs, node, and setup directories
+cp -r ./data $CAULDRON_PATH/data
+cp -r ./docs $CAULDRON_PATH/docs
+cp -r ./node $CAULDRON_PATH/node
+
+# We need to create a few variables to make things easier later
+set -Ux CAULDRON_PALETTES $CAULDRON_PATH/data/palettes.json
+set -Ux CAULDRON_SPINNERS $CAULDRON_PATH/data/spinners.json
+set -Ux CAULDRON_DATABASE $CAULDRON_PATH/data/cauldron.db
+
+# Create the log files
+if not test -f $CAULDRON_PATH/logs/cauldron.log
+  touch $CAULDRON_PATH/logs/cauldron.log
+end
+
+set OS (uname -s)
+
+# If brew is not installed we need it
+if not command -q brew
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+end
+
+# If pipx not installed 
+if not command -q pipx
+if test $OS = "Darwin"
+  brew install pipx
+else
+  sudo apt update
+  sudo apt install pipx
+end
+  pipx ensurepath
+  register-python-argcomplete --shell fish pipx >~/.config/fish/completions/pipx.fish
+  pipx install terminaltexteffects --quiet
+end
+
+if not command -q gum
+  brew install gum
+end
+
+# Now we need to install the dependencies from the ./dependencies.json file
+set apt_dependencies (cat ./dependencies.json | jq -r '.apt[]')
+set brew_dependencies (cat ./dependencies.json | jq -r '.brew[]')
+set snap_dependencies (cat ./dependencies.json | jq -r '.snap[]')
+
+# SQL command to create the 'dependencies' table if it doesn't exist
+set CREATE_TABLE_SQL "CREATE TABLE IF NOT EXISTS dependencies (id INTEGER PRIMARY KEY, name TEXT UNIQUE, version TEXT, date TEXT);"
+
+# Execute the CREATE TABLE command
+sqlite3 $CAULDRON_DATABASE $CREATE_TABLE_SQL
+
+for dep in $apt_dependencies
+  gum spin --spinner moon --title "Installing $dep..." -- fish -c "if not command -q \$dep; sudo apt install \$dep -y; end; if not command -q \$dep; set ERROR_MSG \"Failed to install: \$dep using the command 'sudo apt install \$dep -y'\"; echo \$ERROR_MSG >> \$CAULDRON_PATH/logs/cauldron.log; else; set VERSION (apt show \$dep | grep \"Version\" | cut -d \":\" -f 2 | tr -d \" \"); set DATE (date); sqlite3 \$CAULDRON_DATABASE \"INSERT OR REPLACE INTO dependencies (name, version, date) VALUES ('\$dep', '\$VERSION', '\$DATE')\"; end"
+end
+
+for dep in $brew_dependencies
+  gum spin --spinner moon --title "Installing $dep..." -- fish -c "if not command -q \$dep; brew install \$dep; end; if not command -q \$dep; set ERROR_MSG \"Failed to install: \$dep using the command 'brew install \$dep'\"; echo \$ERROR_MSG >> \$CAULDRON_PATH/logs/cauldron.log; else; set VERSION (brew info \$dep | grep \"version\" | cut -d \" \" -f 1 | tr -d \"version:\"); set DATE (date); sqlite3 \$CAULDRON_DATABASE \"INSERT OR REPLACE INTO dependencies (name, version, date) VALUES ('\$dep', '\$VERSION', '\$DATE');\"; end"
+end
+
+for dep in $snap_dependencies
+  gum spin --spinner moon --title "Installing $dep..." -- fish -c "if not command -q \$dep; sudo snap install \$dep; end; if not command -q \$dep; set ERROR_MSG \"Failed to install: \$dep using the command 'sudo snap install \$dep'\"; echo \$ERROR_MSG >> \$CAULDRON_PATH/logs/cauldron.log; else; set VERSION (snap info \$dep | grep \"installed\" | cut -d \":\" -f 2 | tr -d \" \"); set DATE (date); sqlite3 \$CAULDRON_DATABASE \"INSERT OR REPLACE INTO dependencies (name, version, date) VALUES ('\$$dep', '\$VERSION', '\$DATE');\"; end"
+end
+
+styled-banner "Updated!"
+
+return 0
