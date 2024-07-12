780a4e4e984ba04be7a2fda5c7be50c6fa43c3e6
Abourass <Abourassa@AssetVal.com>
2024-07-12 12:23:32 -0600

    refactor: Improve directory creation and copying logic in cauldron_update.fish

--- internal/cauldron_update.fish
+++ internal/cauldron_update.fish
@@ -1,24 +1,25 @@
 #!/usr/bin/env fish
 
-function cauldron_update
-  set script_dir (dirname (status --current-filename))
+function cauldron_update -d 'Update Cauldron to the latest version'
+  # Path must exist for us to use
+  if not set -q CAULDRON_PATH
+    set -Ux CAULDRON_PATH $HOME/.config/cauldron
+
+    if not test -d $CAULDRON_PATH
+      mkdir -p $CAULDRON_PATH
+    end
+  end
 
   # First we need to make sure the DB exists and the var is set
   if not test -f $CAULDRON_DATABASE
     # If init_cauldron_DB is not defined we need to cpfunc it
     if not functions -q init_cauldron_DB
-      cpfunc $script_dir/internal/init_cauldron_DB.fish
+      cpfunc $CAULDRON_PATH/internal/init_cauldron_DB.fish
     end
 
     # Now we init the cauldron DB
     init_cauldron_DB
   end
-
-  # Copy over the schema file
-  cp $script_dir/data/schema.sql $CAULDRON_PATH/data/schema.sql
-
-  # Invoke the schema just in case it is not there
-  sqlite3 $CAULDRON_DATABASE < $CAULDRON_PATH/data/schema.sql
 
   # Now we need to make sure the DB is up to date
   if test -f $CAULDRON_PATH/data/update.sql
@@ -48,15 +49,6 @@ function cauldron_update
     return 0;
   end
 
-  # Path must exist for us to use
-  if not set -q CAULDRON_PATH
-    set -Ux CAULDRON_PATH $HOME/.config/cauldron
-
-    if not test -d $CAULDRON_PATH
-      mkdir -p $CAULDRON_PATH
-    end
-  end
-
   # Make sure the documentation path is set
   if not set -q __CAULDRON_DOCUMENTATION_PATH
     set -Ux __CAULDRON_DOCUMENTATION_PATH $CAULDRON_PATH/docs
@@ -80,7 +72,7 @@ function cauldron_update
     mkdir -p $tmp_dir/data
 
     # Copy the data folder to the temp folder
-    mv $CAULDRON_PATH/data $tmp_dir/data
+    mv $CAULDRON_PATH/data $tmp_dir
   end
 
   # Now we remove everything in the base folder so we can clone the latest version
@@ -94,7 +86,7 @@ function cauldron_update
     # Make sure the data folder exists
     mkdir -p $CAULDRON_PATH/data
 
-    mv $tmp_dir/data $CAULDRON_PATH/data
+    mv $tmp_dir $CAULDRON_PATH/data
   end
 
   # Now we remove the temp folder
@@ -105,37 +97,22 @@ function cauldron_update
 
   # Copy and source all the needed functions
   for dir in $CAULDRON_LOCAL_DIRS
-    if not test -d $CAULDRON_PATH/$dir
-        mkdir -p $CAULDRON_PATH/$dir
-    end
-
-    cp -r $script_dir/$dir $CAULDRON_PATH/$dir
-    cpfunc $CAULDRON_PATH/$dir -d
+    cpfunc $CAULDRON_PATH/$dir/ -d
   end
 
   # Install the one off scripts that are not part of the main CLI
-  mkdir -p $CAULDRON_PATH/packages
-  cp -r $script_dir/packages $CAULDRON_PATH/packages
-  cpfunc $CAULDRON_PATH/packages/asdf -d
-  cpfunc $CAULDRON_PATH/packages/nvm -d
+  if not test -d $CAULDRON_PATH/packages
+    mkdir -p $CAULDRON_PATH/packages
+  end
+
+  cpfunc $CAULDRON_PATH/packages/asdf/ -d
+  cpfunc $CAULDRON_PATH/packages/nvm/ -d
   cpfunc $CAULDRON_PATH/packages/choose_packman.fish
 
-  # Now we recursively copy the essential doc pieces
-  mkdir -p $CAULDRON_PATH/docs
-  cp -r $script_dir/docs $CAULDRON_PATH/docs
-  mkdir -p $CAULDRON_PATH/node
-  cp -r $script_dir/node $CAULDRON_PATH/node
-
-  # We need to create a few variables to make things easier later
+  # We need to make sure these variables are set
   set -Ux CAULDRON_PALETTES $CAULDRON_PATH/data/palettes.json
   set -Ux CAULDRON_SPINNERS $CAULDRON_PATH/data/spinners.json
   set -Ux CAULDRON_DATABASE $CAULDRON_PATH/data/cauldron.db
-
-  # Create the log files
-  if not test -f $CAULDRON_PATH/logs/cauldron.log
-    mkdir -p $CAULDRON_PATH/logs
-    touch $CAULDRON_PATH/logs/cauldron.log
-  end
 
   set OS (uname -s)
 
@@ -162,9 +139,9 @@ function cauldron_update
   end
 
   # Now we need to install the dependencies from the ./dependencies.json file
-  set apt_dependencies (cat $script_dir/dependencies.json | jq -r '.apt[]')
-  set brew_dependencies (cat $script_dir/dependencies.json | jq -r '.brew[]')
-  set snap_dependencies (cat $script_dir/dependencies.json | jq -r '.snap[]')
+  set apt_dependencies (cat $CAULDRON_PATH/dependencies.json | jq -r '.apt[]')
+  set brew_dependencies (cat $CAULDRON_PATH/dependencies.json | jq -r '.brew[]')
+  set snap_dependencies (cat $CAULDRON_PATH/dependencies.json | jq -r '.snap[]')
 
   for dep in $apt_dependencies
     gum spin --spinner moon --title "Installing $dep..." -- fish -c "if not command -q \$dep; sudo apt install \$dep -y; end; if not command -q \$dep; set ERROR_MSG \"Failed to install: \$dep using the command 'sudo apt install \$dep -y'\"; echo \$ERROR_MSG >> \$CAULDRON_PATH/logs/cauldron.log; else; set VERSION (apt show \$dep | grep \"Version\" | cut -d \":\" -f 2 | tr -d \" \"); set DATE (date); sqlite3 \$CAULDRON_DATABASE \"INSERT OR REPLACE INTO dependencies (name, version, date) VALUES ('\$dep', '\$VERSION', '\$DATE')\"; end"
