d7d39069f4a44e577ad625e446036f7d7ddda836
Abourass <Abourassa@AssetVal.com>
2024-07-12 10:17:08 -0600

    chore: Update asdf_update_go and asdf_update_ruby scripts
    Refactor the asdf_update_go and asdf_update_ruby scripts to use the latest version of the asdf package manager. This update ensures that the Go and Ruby versions are updated to the latest versions available. Additionally, the scripts now set the updated versions as the global versions and check for any local .rb files or .ruby-version file to set the updated versions as the local versions.

--- packages/asdf/asdf_update_go.fish
+++ packages/asdf/asdf_update_go.fish
@@ -1,9 +1,11 @@
 
 function asdf_update_go -d 'Update go to the latest version'
-  asdf install go latest
-  asdf global go latest
-  # Check if there are any local .rb files
-  if test -f go.mod
-    asdf local go latest
+  if contains golang (asdf plugin list)
+    asdf install golang latest
+    asdf global golang latest
+    # Check if there are any local .rb files
+    if test -f go.mod
+      asdf local golang latest
+    end
   end
 end
--- packages/asdf/asdf_update_node.fish
+++ packages/asdf/asdf_update_node.fish
@@ -1,6 +1,35 @@
 #!/usr/bin/env fish
 
 function asdf_update_node -d 'Update Node.js to the latest version'
+  # Version Number
+  set -l func_version "1.0.1"
+  # Flag options
+  set -l options v/version h/help
+  argparse -n asdf_update_node $options -- $argv
+
+  # if they asked the version just return it
+  if set -q _flag_version
+      echo $func_version
+      exit 0
+  end
+
+  # if they asked for help just return it
+  if set -q _flag_help
+      echo "Usage: asdf_update_node"
+      echo "Version: $func_version"
+      echo "Update Node.js to the latest version"
+      echo
+      echo "Options:"
+      echo "  -v, --version  Show the version number"
+      echo "  -h, --help     Show this help message"
+      exit 0
+  end
+
+  if not contains nodejs (asdf plugin list)
+    asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+  end
+
+  if contains nodejs (asdf plugin list)
     asdf install nodejs latest
     asdf global nodejs latest
     corepack enable
@@ -8,12 +37,13 @@ function asdf_update_node -d 'Update Node.js to the latest version'
 
     # Only set the local version if there is a package.json file
     if test -f package.json
-        asdf local nodejs latest
-        yarn set version stable
-        asdf reshim nodejs
+      asdf local nodejs latest
+      yarn set version stable
+      asdf reshim nodejs
     end
 
     # Add back compatibility layers
     npm i tsx@latest -g
     asdf reshim nodejs
+  end
 end
