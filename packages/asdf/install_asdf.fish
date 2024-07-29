8db6d7e3a223774049533108bb213749ebf3c80a
Abourass <Abourassa@AssetVal.com>
2024-07-29 10:46:12 -0600

    refactor: Update ASDF installation logic and add Node.js plugin installation

--- packages/asdf/install_asdf.fish
+++ packages/asdf/install_asdf.fish
@@ -1,18 +1,31 @@
 function install_asdf
-  if command -q asdf
-    return
+  if not command -q asdf
+    installs curl git gpg gawk dirmngr
+    print_center "🎠 Installing ASDF 🎠";
+    git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.0;
+    echo -e "\nsource ~/.asdf/asdf.fish" >>~/.config/fish/config.fish;
+    source ~/.config/fish/config.fish;
+    mkdir -p ~/.config/fish/completions; and ln -s ~/.asdf/completions/asdf.fish ~/.config/fish/completions;
+    print_center "🤹 ASDF Installed 🤹"
+
+    # Set asdf as the preferred version manager
+    set -gx cauldron_packman_pref asdf
   end
 
-  print_center "🎠 Installing ASDF 🎠"
-  brew install asdf
-  echo -e "\nsource "(brew --prefix asdf)"/libexec/asdf.fish" >>~/.config/fish/config.fish
-  source ~/.config/fish/config.fish
-  print_center "🎡 Installing ASDF plugin / dependencies 🎡"
-  installs dirmngr gpg curl gawk
-  print_center "🤹 ASDF Installed 🤹"
-
-  # Set asdf as the preferred version manager
-  set -Ux cauldron_packman_pref asdf
+  # Now we need to see which plugins are installed, and install the ones the user would like
+  if not contains nodejs (asdf plugin list)
+    # Prompt the user and ask if they would like to install nodejs
+    confirm "Would you like cauldron to install the Node.js for you? (This will install the nodejs plugin for asdf)"
+    # Now we check `CAULDRON_LAST_CONFIRM` to see if the user said yes
+    if test $CAULDRON_LAST_CONFIRM = true
+      asdf plugin add nodejs;
+      asdf install nodejs latest;
+      asdf global nodejs latest;
+      corepack enable;
+      asdf reshim nodejs;
+      yarn set version stable;
+    end
+  end
 
   exit 0
 end
