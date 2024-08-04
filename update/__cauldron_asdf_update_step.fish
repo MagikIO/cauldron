725048cd465cc20b6d35c7aae6257df9161b7b42
Abourass <Abourassa@AssetVal.com>
2024-08-04 13:28:38 -0600

    refactor: Update ASDF installation logic and add Node.js plugin installation

--- update/__cauldron_asdf_update_step.fish
+++ update/__cauldron_asdf_update_step.fish
@@ -7,10 +7,29 @@ function __cauldron_asdf_update_step
         if not type -q asdf
             install_asdf
         end
+
+        # Create a log file to pipe the output to
+        mkdir -p $CAULDRON_PATH/logs
+        set log_file $CAULDRON_PATH/logs/asdf_update.txt
+        touch $log_file
+
+        # Reset the log file and add a date stamp
+        echo (date) >$log_file
+
         # Then we need to set the node version
-        gum spin --spinner moon --title "Updating Node..." -- fish -c asdf_update_node
-        gum spin --spinner moon --title "Updating Ruby..." -- fish -c asdf_update_ruby
-        gum spin --spinner moon --title "Updating Go..." -- fish -c asdf_update_go
+        gum spin --spinner moon --title "Updating Node..." -- fish -c __cauldron_asdf_update_node >>$log_file
+        gum spin --spinner moon --title "Updating Ruby..." -- fish -c __cauldron_asdf_update_ruby >>$log_file
+        gum spin --spinner moon --title "Updating Go..." -- fish -c __cauldron_asdf_update_go >>$log_file
+
+        # Add Instruction to close the pager
+        echo ""
+        echo "Press 'q' to close this and continue" >>$log_file
+
+        # Show the user a pager with the output
+        shiny pager <$log_file
+
+        # Remove the log file
+        rm $log_file
     end
 
     # If they prefer nvm
