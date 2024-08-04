ac1bacbf602cf414d92c63649dd531524ada837d
Abourass <Abourassa@AssetVal.com>
2024-08-04 13:30:35 -0600

    refactor: Update ASDF installation logic and add Node.js plugin installation

--- update/__cauldron_asdf_update_step.fish
+++ update/__cauldron_asdf_update_step.fish
@@ -3,11 +3,6 @@
 function __cauldron_asdf_update_step
     # If asdf is their preferred version manager, we need to make sure it's installed and set the node version
     if test $cauldron_packman_pref = asdf; or test $cauldron_packman_pref = none; or test $cauldron_packman_pref = asdf_preferred
-        # First we need to see if they have asdf installed
-        if not type -q asdf
-            install_asdf
-        end
-
         # Create a log file to pipe the output to
         mkdir -p $CAULDRON_PATH/logs
         set log_file $CAULDRON_PATH/logs/asdf_update.txt
@@ -15,6 +10,11 @@ function __cauldron_asdf_update_step
 
         # Reset the log file and add a date stamp
         echo (date) >$log_file
+
+        # First we need to see if they have asdf installed
+        if not type -q asdf
+            install_asdf | tee -a $log_file
+        end
 
         # Then we need to set the node version
         gum spin --spinner moon --title "Updating Node..." -- fish -c __cauldron_asdf_update_node >>$log_file
