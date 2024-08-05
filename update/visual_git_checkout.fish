03b6f3a643fbbf333db57001e4e96c7149f75ed4
Abourass <Abourassa@AssetVal.com>
2024-08-05 12:07:02 -0600

    refactor: Update visual_git_checkout.fish to use 'tr' instead of 'echo' for branch selection

--- update/visual_git_checkout.fish
+++ update/visual_git_checkout.fish
@@ -5,7 +5,7 @@ function visual_git_checkout
     set branches (git branch --all | sed 's|^.*/||' | sed 's/^[[:space:]]*//;s/[[:space:]]*$//' | grep -E '^[^*]' | sort -u)
 
     # Use fzf to select a branch
-    set selected_branch (echo $branches | fzf --preview "git show --color=always --stat {}" --preview-window="right:wrap")
+    set selected_branch (echo $branches | tr ' ' '\n' | fzf --preview "git show --color=always --stat {}" --preview-window="right:wrap")
 
     if test -n "$selected_branch"
         git checkout "$selected_branch"
