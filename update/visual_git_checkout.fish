8e591386a917755c063805e320fd56331c691a81
Abourass <Abourassa@AssetVal.com>
2024-08-05 11:59:14 -0600

    refactor: Improve visual_git_checkout function for easier branch selection

--- update/visual_git_checkout.fish
+++ update/visual_git_checkout.fish
@@ -2,10 +2,7 @@
 
 function visual_git_checkout
     # Get all branches, remove remote prefixes, and trim whitespace
-    set branches (git branch --all | string replace -r "^.*\/" "" | string trim | sort -u)
-
-    # Remove the asterisk from the current branch
-    set branches (for branch in $branches; echo $branch | sed "s/^\* //g"; end | sort -u)
+    set branches (git branch --all | sed 's|^.*/||' | sed 's/^[[:space:]]*//;s/[[:space:]]*$//' | grep -E '^[^*]' | sort -u)
 
     # Use fzf to select a branch
     set selected_branch (echo $branches | fzf --preview "git show --color=always --stat {}" --preview-window="right:wrap")
