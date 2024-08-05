53782b1322db5e2f41219050992512a3d65e03d5
Abourass <Abourassa@AssetVal.com>
2024-08-05 11:43:09 -0600

    refactor: Improve directory creation and copying logic in cauldron_update.fish, update Homebrew and yarn in update_repo.fish, update package manager preference logic in choose_packman.fish, update ASDF installation logic and add Node.js plugin installation, add visual_git_checkout function for easier branch selection, improve visual_git_checkout function for easier branch selection, update Aquarium installation logic and add version check, update getLatestGithubReleaseTag function to handle empty target and improve error handling, update update_repo function to include ASDF and Git updates, include "update" folder in CAULDRON_LOCAL_DIRS, update Cauldron update script to include version check and help option, update package.json script to remove unnecessary tag argument

--- update/cauldron_update.fish
+++ update/cauldron_update.fish
@@ -176,9 +176,86 @@ function cauldron_update -d 'Update Cauldron to the latest version'
   # We need to make sure these variables are set
   set -Ux CAULDRON_PALETTES $CAULDRON_PATH/data/palettes.json
   set -Ux CAULDRON_SPINNERS $CAULDRON_PATH/data/spinners.json
-  set -Ux CAULDRON_DATABASE $CAULDRON_PATH/data/cauldron.db
 
-  ./$CAULDRON_PATH/tools/__install_essential_tools.fish
+  set OS (uname -s)
+
+  # If brew is not installed we need it
+  if not command -q brew
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  end
+
+  # If pipx not installed 
+  if not command -q pipx
+    if test $OS = "Darwin"
+      brew install pipx
+    else
+      sudo apt update
+      sudo apt install pipx
+    end
+      pipx ensurepath
+      register-python-argcomplete --shell fish pipx >~/.config/fish/completions/pipx.fish
+  end
+
+  if not command -q tte
+    pipx ensurepath
+    pipx install terminaltexteffects --quiet
+  end
+
+  if not command -q gum
+    brew install gum
+  end
+
+  # As long as their is a dependencies.json file we will install the dependencies
+    if test -f $CAULDRON_PATH/dependencies.json
+      set apt_dependencies (cat $CAULDRON_PATH/dependencies.json | jq -r '.apt[]')
+      set brew_dependencies (cat $CAULDRON_PATH/dependencies.json | jq -r '.brew[]')
+      set snap_dependencies (cat $CAULDRON_PATH/dependencies.json | jq -r '.snap[]')
+  
+      for dep in $apt_dependencies
+          gum spin --spinner moon --title "Installing $dep..." -- fish -c "
+              if not type -q $dep
+                  sudo apt install $dep -y
+              end
+              if not type -q $dep
+                  set ERROR_MSG \"Failed to install: $dep using the command 'sudo apt install $dep -y'\"
+                  echo $ERROR_MSG >> $CAULDRON_PATH/logs/cauldron.log
+              else
+                  set VERSION (apt show $dep | grep 'Version' | cut -d ':' -f 2 | tr -d ' ')
+                  set DATE (date)
+                  sqlite3 $CAULDRON_DATABASE \"INSERT OR REPLACE INTO dependencies (name, version, date) VALUES ('$dep', '$VERSION', '$DATE')\"
+              end"
+      end
+      
+      for dep in $brew_dependencies
+          gum spin --spinner moon --title "Installing $dep..." -- fish -c "
+              if not type -q $dep
+                  brew install $dep
+              end
+              if not type -q $dep
+                  set ERROR_MSG \"Failed to install: $dep using the command 'brew install $dep'\"
+                  echo $ERROR_MSG >> $CAULDRON_PATH/logs/cauldron.log
+              else
+                  set VERSION (brew info $dep | grep 'version' | cut -d ' ' -f 1 | tr -d 'version:')
+                  set DATE (date)
+                  sqlite3 $CAULDRON_DATABASE \"INSERT OR REPLACE INTO dependencies (name, version, date) VALUES ('$dep', '$VERSION', '$DATE')\"
+              end"
+      end
+      
+      for dep in $snap_dependencies
+          gum spin --spinner moon --title "Installing $dep..." -- fish -c "
+              if not type -q $dep
+                  sudo snap install $dep
+              end
+              if not type -q $dep
+                  set ERROR_MSG \"Failed to install: $dep using the command 'sudo snap install $dep'\"
+                  echo $ERROR_MSG >> $CAULDRON_PATH/logs/cauldron.log
+              else
+                  set VERSION (snap info $dep | grep 'installed' | cut -d ':' -f 2 | tr -d ' ')
+                  set DATE (date)
+                  sqlite3 $CAULDRON_DATABASE \"INSERT OR REPLACE INTO dependencies (name, version, date) VALUES ('$dep', '$VERSION', '$DATE')\"
+              end"
+      end
+  end
 
   # Now we need to update the DB's version
   sqlite3 $CAULDRON_DATABASE "INSERT OR REPLACE INTO cauldron (version) VALUES ('$LATEST_VERSION')"
