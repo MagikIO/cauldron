6e290bd44f0a0649a62c8dd5375528636054e25a
Abourass <Abourassa@AssetVal.com>
2024-08-05 13:02:50 -0600

    Add new familiars to the database

---
+++ data/update.sql
@@ -0,0 +1,5 @@
+INSERT INTO "familiars" ("id", "name", "display_name", "familiar_type", "unlocked", "cow_src_ext", "nickname") VALUES ('8', 'vault-boy', NULL, 'Meme', '1', '1', NULL);
+INSERT INTO "familiars" ("id", "name", "display_name", "familiar_type", "unlocked", "cow_src_ext", "nickname") VALUES ('9', 'wheatley', NULL, 'Meme', '1', '1', NULL);
+INSERT INTO "familiars" ("id", "name", "display_name", "familiar_type", "unlocked", "cow_src_ext", "nickname") VALUES ('10', 'wilfred', NULL, 'Meme', '1', '1', NULL);
+INSERT INTO "familiars" ("id", "name", "display_name", "familiar_type", "unlocked", "cow_src_ext", "nickname") VALUES ('11', 'woodstock', NULL, 'Meme', '1', '1', NULL);
+INSERT INTO "familiars" ("id", "name", "display_name", "familiar_type", "unlocked", "cow_src_ext", "nickname") VALUES ('12', 'yoda', NULL, 'Meme', '1', '1', NULL);
