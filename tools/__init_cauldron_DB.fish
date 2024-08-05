3d5b9b78fbae75280ce209f35e7805c46e9c990f
Abourass <Abourassa@AssetVal.com>
2024-08-05 13:58:32 -0600

    refactor: Update cauldron database initialization script
    - Use `/dev/null` to suppress error output when running schema and update SQL scripts
    - Insert or replace the most recent version into the cauldron table
    
    Refactor related commits:
    - Update __list_familiars.fish to use universal variable for CAULDRON_FAMILIAR
    - Reorder apt dependencies in dependencies.json
    - Update cauldron_update.fish to include version check and improve branch selection logic
    - Update dependencies.json to reorder apt dependencies
    - Improve version check and branch selection logic in cauldron_update.fish
    - Remove 'rig' from brew dependencies and add it to cauldron_update.fish
    - Update familiars.fish to use correct path for __list_familiars.fish
    - Add new familiars to the database

--- tools/__init_cauldron_DB.fish
+++ tools/__init_cauldron_DB.fish
@@ -10,15 +10,15 @@ if not test -f $CAULDRON_DATABASE
   touch $CAULDRON_DATABASE
 end
 
-sqlite3 $CAULDRON_DATABASE < $CAULDRON_PATH/data/schema.sql
+sqlite3 $CAULDRON_DATABASE < $CAULDRON_PATH/data/schema.sql 2> /dev/null
 
 # Now we need to make sure the DB is up to date
 if test -f $CAULDRON_PATH/data/update.sql
-  sqlite3 $CAULDRON_DATABASE < $CAULDRON_PATH/data/update.sql
+  sqlite3 $CAULDRON_DATABASE < $CAULDRON_PATH/data/update.sql 2> /dev/null
 end
 
 # Make sure add the most recent version to the DB
 set currVersion (git ls-remote --tags $CAULDRON_GIT_REPO | awk '{print $2}' | grep -o "v[0-9]*\.[0-9]*\.[0-9]*" | sort -V | tail -n 1 | sed 's/v//')
-sqlite3 $CAULDRON_DATABASE "INSERT INTO cauldron (version) VALUES ('$currVersion')"
+sqlite3 $CAULDRON_DATABASE "INSERT OR REPLACE INTO cauldron (version) VALUES ('$currVersion')"
 
 
