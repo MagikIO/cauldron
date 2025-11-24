6021dc1af83fe27dae0765ace3174bba024b0d75
Abourass <Abourassa@AssetVal.com>
2025-11-24 12:50:47 -0700

    fix: replace exit with return in cauldron function for consistent flow control

--- cli/cauldron.fish
+++ cli/cauldron.fish
@@ -18,31 +18,31 @@ function cauldron
   # If the user passes the -v or --version flag, print the version number
   if set -q _flag_v; or set -q _flag_version
     echo "v$func_version"
-    exit 0
+    return 0
   end
 
   # If they asked for the category return it
   if set -q _flag_z; or set -q _flag_cauldron
     echo $cauldron_category
-    exit 0
+    return 0
   end
 
   # If the user passes the -h or --help flag, run __cauldron_help
   if set -q _flag_h; or set -q _flag_help
     __cauldron_help
-    exit 0
+    return 0
   end
 
   # If the user passes the -D or --new-docs flag, run __cauldron_install_help
   if set -q _flag_D; or set -q _flag_new-docs
     __cauldron_install_help --src $_flag_D
-    exit 0
+    return 0
   end
 
   # If the user passes the -u or --update flag, run __cauldron_update
   if set -q _flag_u; or set -q _flag_update
     cauldron_update
-    exit 0
+    return 0
   end
 
   # If the user passes the -t or --tmux flag, run cauldron_tmux
@@ -64,7 +64,7 @@ function cauldron
         # If no specific action or invalid action, show interactive menu
         cauldron_tmux
     end
-    exit 0
+    return 0
   end
 
   # If the user passes no flags, run the main function
